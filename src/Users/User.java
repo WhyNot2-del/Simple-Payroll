4f2cda227fe030663a97f05f3cd2c380853ecae3
Christopher Waschke <chrisdwaschke@gmail.com>
2025-02-02 10:42:44 -0800

    Added base of User abstract class.

--- src/Users/User.java
+++ src/Users/User.java
@@ -1,5 +1,31 @@
 package Users;
 
 abstract class User{
+    private String username;
+    private String password;
+    private String email;
+    private String ssn;
+
+    boolean checkPassword(String incomingPassword){
+        //TODO: Stub of method, add logic for checking password.
+        return false;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSSN(String newSSN){
+        this.ssn = newSSN;
+    }
 
 }
