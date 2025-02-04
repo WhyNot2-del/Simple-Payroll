64b2a98386a5d1db7a35f7aeadcbcf66ea555e3c
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-02-03 17:52:41 -0800

    modified parameters for methods

--- src/UserManager/UserManager.java
+++ src/UserManager/UserManager.java
@@ -1,49 +1,44 @@
 package UserManager;
 
+import Users.*;
 import java.util.ArrayList;
 
 class UserManager {
     
-    public void spendPaycheck(ArrayList<User> users, int Employee, int spentPay) {
+    public void spendPaycheck(ArrayList<RegUser> users, int Employee, int spentPay) {
         users.get(Employee).setWallet(users.get(Employee).getWallet() - spentPay);
     } //  ends spendPaycheck method
 } //  ends UserManager class
 
 class AdminManager {
 
-    public void modifyEmployeeUsername(ArrayList<User> users, String name, int employee) {
-        users.get(employee).setUserName(name);
+    public void modifyEmployeeUsername(User user, String name) {
+        user.setUsername(name);
     } //  ends modifyEmployeeUsername method
 
-    public void modifyEmployeePass(ArrayList<User> users, String pass, int employee) {
-        users.get(employee).setPassword(pass);
+    public void modifyEmployeePass(User user, String pass) throws Exception {
+        user.setPassword(pass);
     } //  ends modifyEmployeePass method
 
-    public void modifyEmployeeEmail(ArrayList<User> users, String email, int employee) {
-        users.get(employee).setEmail(email);
+    public void modifyEmployeeEmail(User user, String email) {
+        user.setEmail(email);
     } //  ends modifyEmployeeEmail method
 
-    public void modifyEmployeePayRate(ArrayList<User> users, String payRate, int employee) {
-        users.get(employee).setPayRate(payRate);
+    public void modifyEmployeePayRate(RegUser user, double payRate) {
+        user.setPayRate(payRate);
     } //  ends modifyEmployeePayrate method
 
-    public void modifyEmployeeWallet(ArrayList<User> users, double wallet, int employee) {
-        users.get(employee).setWallet(wallet);
-        /* 
-         * Possible better version of Wallet/paying employees. 
-         * In the test class, prompt admin to input the # of hours employee worked which would then be passed.
-         * In the manager class, multiply the employee's payrate, hours worked, and 0.88 (income tax) to generate what will be added 
-         * users.get(Employee).setWallet(users.get(Employee).getWallet() + (users.get(Employee).getPayRate * hours * 0.88));
-         */
+    public void modifyEmployeeWallet(RegUser user, double hours) {
+        user.setWallet(user.getWallet() + (user.getPayRate() * hours * 0.88));
     } //  ends modifyEmployeePayrate method
 
     // creates a new regUser object and places it into the users arraylist
-    public void addEmployee(ArrayList<User> users, String email, String name, String ssn, String pass, double payRate) {
-        users.add(new regUser(name, pass, email, ssn, payRate));
+    public void addEmployee(ArrayList<User> users, String email, String name, String ssn, String pass, double payRate) throws Exception {
+        users.add(new RegUser(name, pass, email, ssn, payRate));
     } //  ends addEmployee method
 
     //  removes specified user object from the users arraylist
-    public void removeEmployee(ArrayList<User> users, int employee) {
-        users.remove(employee);
+    public void removeEmployee(ArrayList<User> users, RegUser user) {
+        users.remove(user);
     } //  ends removeEmployee method
 } //  ends AdminManager class
