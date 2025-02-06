a69c89f1e779a9cc1e5613a265d1c624ac2e2f3d
Unfound999 <jacksocrod@gmail.com>
2025-02-05 20:30:12 -0800

    Added comments

--- src/App.java
+++ src/App.java
@@ -6,21 +6,26 @@ import UserManager.*;
 
 public class App {
 
+    //Creating an arraylist to use throughout the program, holding user nessesary information
     private final static ArrayList<User> employees = new ArrayList<User>();
 
     public static void main(String[] args) throws Exception {
 
+        //declaring variables and creating a scanner object
         Scanner input = new Scanner(System.in);
         boolean isAdmin = false;
         User currentUser = null;
 
+        //Must create an account every time for this program 
         System.out.println("Create an account for demo.");
         createAdminUser();
 
+        //login statments and pulling input
         System.out.println("Hello! I hope your day is going well, it's time to log in!");
         System.out.println("Are you an admin or a regular user?(Please enter 'admin', or 'reguser')");
         String adminoruser = input.nextLine();
         
+        //Switch within a trycatch. Switch checks the users input to decide if they are an admin or a user, and then logs them in accordingly.
         try {
             switch(adminoruser.toLowerCase()) {
                 
@@ -31,6 +36,8 @@ public class App {
         } catch (NoSuchAlgorithmException e) {
         }
 
+        //Do while with an if()else inside that contains menu options for admin users and if the user is not an admin, executes
+        //the regular users options.
         if(isAdmin) {
             
             boolean running = true;
@@ -66,6 +73,7 @@ public class App {
         }
     }
 
+    //Method to grab a user out of the list of users
     public static User getUser() {
         Scanner input = new Scanner(System.in);
         boolean running = true;
@@ -82,6 +90,7 @@ public class App {
         return null;
     }
 
+    //Method to spend a regular users paycheck
     public static void spendPaycheck() {
         User employee = getUser();
         if(employee.getClass() == RegUser.class){
@@ -90,6 +99,8 @@ public class App {
         System.out.println("User must be a regular user to be paid! Admins work for free.");
     }
 
+    //Method createUser that throws two exceptions to catch hashing and salting of passwords and if the password has incorrect characters in it
+    //The main purpose of this method is to ask the user for necessary information, and then send that information to the manager class.
     public static void createUser() throws BadPasswordException, NoSuchAlgorithmException {
 
         Scanner input = new Scanner(System.in);
@@ -118,6 +129,7 @@ public class App {
 
     }
 
+    //Method to create a user of type admin; same as previous method but for an admin user
     public static void createAdminUser() throws BadPasswordException, NoSuchAlgorithmException {
 
         Scanner input = new Scanner(System.in);
@@ -141,6 +153,7 @@ public class App {
         AdminManager.addAdmin(employees, userEmail, userUsername, userSSN, userPassword, payfundAmount);
     }
 
+    //Method to view the current list of employees held in the array list
     public static void viewEmployeeList() {
 
         for(int i = 0; i < employees.size(); i++ ) {
@@ -150,6 +163,7 @@ public class App {
         }  
     }
 
+    //Method to view a specific employee within the Employees arraylist
     public static void viewEmployee() {
        
         Scanner input = new Scanner(System.in);
@@ -173,6 +187,8 @@ public class App {
         }
     }
 
+    //Method to pay a regular user that requires the user to be an admin (because it's only an option in the admin menu) and for the user to input the hours of the employee.
+    //It checks for if the person inputs a negative number of hours
     public static void payUser() {   
         
         Scanner input = new Scanner(System.in);
@@ -183,7 +199,7 @@ public class App {
             return;
         }
 
-        System.out.print("Hello, please enter employee hours: ");
+        System.out.print("Hello, please enter the employees hours: ");
         Double hours = input.nextDouble();
 
         if(hours < 0) {
@@ -195,6 +211,7 @@ public class App {
         }
     }
 
+    //This method prompts a user for their username and passwords and then checks the arraylist 'employees' against the imputed information
     public static RegUser logInRegUser() throws NoSuchAlgorithmException {
 
         Scanner input = new Scanner(System.in);
@@ -214,13 +231,14 @@ public class App {
                 }
             }
         }
+        input.close();
         return null;
     }
 
+    //Method that is similar to the one above accept this method is to log in an admin
     public static AdminUser logInAdmin() throws NoSuchAlgorithmException {
 
         Scanner input = new Scanner(System.in);
-        boolean passwordTrue = false;
 
         System.out.print("Hello, please enter your username!: ");
         String username = input.nextLine();
@@ -238,7 +256,8 @@ public class App {
             }
         } 
         System.out.println("Unable to find user!");
+        input.close();
         return null;
-        
     }
+    
 }
